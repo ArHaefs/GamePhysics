9c55ac7184fdd1d14eb9b7980973ce4ff75d9bde
Arne <arhaefs@stud.mediadesign.de>
2020-01-10 11:12:51 +0100

    First

---
+++ Physics/Assets/Scripts/MovementTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementTest : MonoBehaviour
+{
+    Vector3 movement;
+    [SerializeField]
+    Rigidbody body;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void changeMovementForce(Vector3 vect)
+    {
+        movement += vect;
+    }
+
+    public Vector3 retMovement()
+    {
+        return movement;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(movement);
+        body.AddForce(movement, ForceMode.Force);
+    }
+}
---
+++ Physics/Assets/Scripts/MoverFloor.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverFloor : MonoBehaviour
+{
+
+    [SerializeField]
+    [Range(-10, 10)]
+    int downAndUp, leftAndRight;
+
+    GameObject Moving;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<MovementTest>())
+        {
+            collision.gameObject.GetComponent<MovementTest>().changeMovementForce(new Vector3(leftAndRight, 0, downAndUp));
+            Moving = collision.gameObject;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<MovementTest>())
+        {
+            collision.gameObject.GetComponent<MovementTest>().changeMovementForce(new Vector3(-leftAndRight, 0, -downAndUp));
+            Moving = null;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
